8349e0fa21d41bacfff5728ed4f84e30bb8294a3
Yury Nascimento <yurynascimento96@gmail.com>
2020-06-30 13:39:01 -0300

    version 1

---
+++ dados.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2020 at 04:59 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(250) NOT NULL,
+  `SENHA` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`ID`, `NOME`, `SENHA`) VALUES
+(1, 'Exemplo', 'exemplo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head></head>
+<body>
+    <div>Cadastro</div><br>
+    <!-- Formulario para inserção de dados no Banco 'Dados' -->
+    <form name="form1" method="POST" action="salvar.php">
+    Nome:<br><input type="text" name="nome" required><br>
+    Senha:<br><input type="password" name="senha" required>
+    <input type="submit" value="Salvar"><br>
+    </form>
+    <table border=1><tr><td>ID</td><td>NOME</td><td>SENHA</td><td><center>X</center></td><td><center>----</center></td></tr>
+
+    <!-- "Select" direcionando todos os dados do Banco 'Dados' para uma tabela -->
+    <?php
+        $connect = mysqli_connect('localhost', 'root', null, 'Dados');
+            $query = "Select * from usuario;";
+            $select = $connect -> query($query);
+            while ($resultado = $select -> fetch_assoc()) {
+            echo '<tr><td>'.$resultado['ID'].'</td>
+            <td>'.$resultado['NOME'].'</td>
+            <td>'.$resultado['SENHA'].'</td>
+            <td><a href="delete.php?id='.$resultado['ID'].'">Excluir</a></td>
+            <td><a href="atualizar.php?id='.$resultado['ID'].'">Atualizar</a></td></tr>';
+            }
+            ?>
+        </table>
+</body>
+</html>
